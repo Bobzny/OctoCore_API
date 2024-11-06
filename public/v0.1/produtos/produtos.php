0dfc230ad1d00b143d5f2fe58d15586a9293d326
Eduardo Almeida <bobumapessoa@gmail.com>
2024-11-06 11:23:28 -0300

    consulta de produtos melhorada
    -agora a pesquisa de produtos busca corretamente por nome da categoria
    -inclusão de json de testes para a aula de front

--- public/v0.1/produtos/produtos.php
+++ public/v0.1/produtos/produtos.php
@@ -3,12 +3,44 @@ require_once __DIR__ ."/../../../api_core/query.php";
 require_once __DIR__ ."/../../../api_core/response.php";
 
 $categoria = $_GET['categoria'];
-
-$resultados = Query::Send("SELECT * FROM PRODUTOS WHERE idCategoria = 1");
+#Comando sql que da join das duas tabelas com base em id de categoria e depois filtra pelo nome especificado na url
+$resultados = Query::Send(" 
+    SELECT p.* 
+    FROM PRODUTOS p
+    INNER JOIN CATEGORIAS c ON p.idCategoria = c.idCategoria
+    WHERE c.nome = '$categoria'
+");
 
 echo Response::geison($resultados);
 
-print_r($resultados[0])
+/*{ JSON de resposta para testes
+    "status": 200,
+    "message": "success",
+    "api_version": "0.2",
+    "time_response": 1730897889,
+    "datetime_response": "06-11-2024 13:11:09",
+    "data": [
+        {
+            "idProduto": "1",
+            "nome": "Raizen",
+            "valorUnitario": "2400.99",
+            "quantidade": "50",
+            "descricao": "Potente",
+            "linkImagem": null,
+            "idCategoria": "1"
+        },
+        {
+            "idProduto": "2",
+            "nome": "i9",
+            "valorUnitario": "2500.99",
+            "quantidade": "10",
+            "descricao": "Potente 2",
+            "linkImagem": null,
+            "idCategoria": "1"
+        }
+    ]
+}*/
+
 
 
 
--- public/v0.1/register/register.php
+++ public/v0.1/register/register.php
@@ -8,21 +8,20 @@ $data = json_decode($requisicao, true);
 
 
 if(!empty($data)){
-    try{
+
+    if (isset($data['user']) && isset($data['password'])){ #verifica se o JSON possui os campos necessários
 
         $user = $data['user'];
         $pw = $data['password'];
+        #Isso tem que ser mudado pra um prepared statement no futuro pra não ter injeção sql, por enquanto serve
+        $resultado = Query::Send("INSERT INTO USUARIOS VALUES (null, '$user', '$pw', default, 0)");  
+        echo Response::geison($resultado); 
 
-
-    }finally{
-        die("Requisição inválida!");
     }
-
-
-    $resultado = Query::Send("INSERT INTO USUARIOS VALUES (null, '$user', '$pw', default, 0)"); 
-    echo Response::geison($resultado);
+    else{
+        echo "Requisição inválida";
+    }
     
-   
 }                                                                                       
 else{
     echo "Sem JSON na requisição :C";
