eed2c5eafa7630963ff94adc9743635a588dbed6
Eduardo Almeida <bobumapessoa@gmail.com>
2024-11-13 11:32:02 -0300

    POST de produtos
    -comecei a implementar post para cadastro de produtos

--- endpoints/produtos/produtos.php
+++ endpoints/produtos/produtos.php
@@ -2,14 +2,18 @@
 require_once __DIR__ ."/../../core/query.php";
 require_once __DIR__ ."/../../core/response.php";
 
-$requisicao = file_get_contents('php://input');
-$data = json_decode($requisicao, true);
 
 print_r($data);
 
+if(isset($_POST)){
+    $requisicao = file_get_contents('php://input');
+    $data = json_decode($requisicao, true);
+    $param = [$_POST['nome'], $_POST['valorUnitario'], $_POST['descricao'],$_POST['linkImagem'], $_POST['idCategoria']];
+    Query::Send("INSERT INTO PRODUTOS (idProduto, nome, valorUnitario, quantidade, descricao, linkImagem, idCategoria) VALUES (null, ?, ?, 10, ?, ?, ?)", $param);
 
+}
 
-if (isset($_GET['categoria'])){
+else if (isset($_GET['categoria'])){
     $categoria = array($_GET['categoria']);
     #Comando sql que da join das duas tabelas com base em id de categoria e depois filtra pelo nome especificado na url
     $resultados = Query::Send(" 
@@ -20,7 +24,7 @@ if (isset($_GET['categoria'])){
     ",$categoria);
 
     
-}if(isset($_GET['busca'])){
+}else if(isset($_GET['busca'])){
     $busca = array($_GET['busca']);
     $resultados = Query::Search("produtos", $busca);
 }
